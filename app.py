2c7d26cdc0e2ccd5c8c9e77086440e36e199506e
Chiranjeevi Varma <70083124+varmapenmatsa4567@users.noreply.github.com>
2022-02-01 16:52:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import *
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
+
+@app.route("/")
+def home():
+    todos = Todo.query.all()
+    return render_template("index.html",todo_list = todos)
+
+@app.route("/add",methods=['POST'])
+def add():
+    title = request.form["data"]
+    todo = Todo(title=title,complete=False)
+    db.session.add(todo)
+    db.session.commit()
+    todos = Todo.query.all()
+    print(todos)
+    return jsonify({'data': render_template("content.html",todo_list=todos)})
+
+@app.route("/update",methods=['POST'])
+def update():
+    id = request.form["data"]
+    todo = Todo.query.filter_by(id=id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    todos = Todo.query.all()
+    print(todos)
+    return jsonify({'data': render_template("content.html",todo_list=todos)})
+
+@app.route("/delete",methods=['POST'])
+def delete():
+    id = request.form["data"]
+    todo = Todo.query.filter_by(id=id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    todos = Todo.query.all()
+    print(todos)
+    return jsonify({'data': render_template("content.html",todo_list=todos)})
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
